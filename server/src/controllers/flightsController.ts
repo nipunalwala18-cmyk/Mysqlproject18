01b7514932f294d25bc8ebb66ea0d570d4788bcb
nipunalwala18-cmyk <nipunalwala18@gmail.com>
2025-11-14 11:01:50 +0530

    Delete server directory

--- server/src/controllers/flightsController.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "../config/db";
-import { Request, Response } from "express";
-
-export const getFlights = async (req: Request, res: Response) => {
-  const [rows] = await db.query("SELECT * FROM flights");
-  res.json(rows);
-};
--- server/src/routes/flights.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getFlights } from "../controllers/flightsController";
-
-const router = express.Router();
-router.get("/", getFlights);
-
-export default router;
